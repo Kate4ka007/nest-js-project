d0eff4f8fafe3c952acabea1baf95caf8cf31bc9
Kate4ka007 <shlemena@mail.ru>
2022-10-21 12:01:02 +0300

    feat: add methods

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { DeleteController } from './items/delete.controller';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
+import { ProductsController } from './products/products.controller';
 
 @Module({
   imports: [],
-  controllers: [ItemsController, DeleteController],
+  controllers: [ItemsController, DeleteController, ProductsController],
   providers: [ItemsService],
 })
 export class AppModule {}
--- src/items/examples/example.ts
+++ src/items/examples/example.ts
@@ -3,6 +3,11 @@ fetch('https://nest-server-kate4ka.herokuapp.com/items')
   .then((data) => console.log(data))
   .catch((err) => console.error(err));
 
+fetch('https://web-production-c91c.up.railway.app/items')
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
+
 fetch('https://nest-server-kate4ka.herokuapp.com/items', {
   method: 'POST',
   headers: {
@@ -19,6 +24,23 @@ fetch('https://nest-server-kate4ka.herokuapp.com/items', {
   .then((data) => console.log(data))
   .catch((err) => console.error(err));
 
+fetch('https://web-production-c91c.up.railway.app/items', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    title: 'trinket',
+    description: 'whatever',
+    img: 'png',
+    link: 'png',
+  }),
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
+
 fetch('https://nest-server-kate4ka.herokuapp.com/delete', {
   method: 'POST',
   headers: {
@@ -26,6 +48,18 @@ fetch('https://nest-server-kate4ka.herokuapp.com/delete', {
     'Content-Type': 'application/json',
   },
   body: JSON.stringify({ name: 'Din' }),
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.error(err));
+
+fetch('https://web-production-c91c.up.railway.app/delete', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ title: 'Din' }),
 })
   .then((response) => response.json())
   .then((data) => console.log(data))
